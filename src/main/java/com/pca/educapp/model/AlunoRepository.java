5a22fb3143b88a39218fa7f105416b456b50a6d7
Icaro Lessa <46797949+icaroless@users.noreply.github.com>
2021-04-12 01:17:39 -0300

    Delete src directory

--- src/main/java/com/pca/educapp/model/AlunoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pca.educapp.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-    Aluno findByNome(String nome);
-}
